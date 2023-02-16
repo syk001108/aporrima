80d2e19144f6ba7b78564dc319307718c02d0219
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-16 16:36:19 +0000

    FEAT: Create Script for setting worker node
    Create setting-worker.sh for setting up standalone
    cluster's worker node

---
+++ spark/setting-worker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo chmod -R 777 ./aporrima
+sudo apt-get update
+sudo apt install openssh-server openssh-client -y
+sudo apt install net-tools
+
+sudo sed -i "/PasswordAuthentication/ c\PasswordAuthentication yes" /etc/ssh/sshd_config
+sudo sed -i "/PermitRootLogin/ c\PermitRootLogin yes" /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+echo "This script will install a JAVA&PYTHON3 for Spark"
+read -r -p "Proceed? [y/N] " response
+if [[ ! $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    echo "Aborting..."
+    exit 1
+fi
+
+echo "Start install JAVA"
+./aporrima/spark/install-java.sh
+
+sleep 1
+
+echo "Start install Python3"
+./aporrima/spark/install-java.sh
