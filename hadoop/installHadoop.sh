d1960b52c6f0f5e3720e875687a423f17459ea13
psy337337 <psy337337@naver.com>
2023-02-17 13:56:08 +0900

    Update installHadoop.sh

--- hadoop/installHadoop.sh
+++ hadoop/installHadoop.sh
@@ -6,5 +6,5 @@ wget https://downloads.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz
 tar xzf hadoop-3.3.4.tar.gz
 
 
-cat bashrc.txt >> /home/hadoop/.bashrc
+cat ./aporrima/hadoop/bashrc.txt >> /home/hadoop/.bashrc
 source ~/.bashrc
