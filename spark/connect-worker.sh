f5e4ed7d92767621636e3f4c3e8f1ea9edeafbff
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-21 16:33:12 +0000

    STYLE: Change the worker node hostname to spark
    Change the worker node hostname to spark

--- spark/connect-worker.sh
+++ spark/connect-worker.sh
@@ -6,7 +6,7 @@ cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
 
 cnt=0
-for i in $(sed -n '/worker/p' /etc/hosts)
+for i in $(sed -n '/spark/p' /etc/hosts)
 do
 	cnt=$(($cnt+1))
 	if [[ "${i} " == *"$(hostname -I)"* ]] || [ $(($cnt%2)) == 0 ];then
