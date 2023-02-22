c3f8882b6c5fb2b52805bd9f38ef6edf7b13e7a3
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-22 17:37:08 +0000

    STYLE: Commit for setting-standalone test
    Commit for setting-standalone test

--- spark/setting-standalone.sh
+++ spark/setting-standalone.sh
@@ -4,7 +4,7 @@ MASTER_IP=$(/sbin/ifconfig | grep '\<inet\>' | sed -n '1p' | tr -s ' ' | cut -d 
 MASTER_HOST=$(hostname)
 
 sudo mkdir /home/spark/logs
-sudo cd $SPARK_HOME
+sudo cd ./spark
 pwd
 
 cp spark-defaults.conf.template spark-defaults.conf
