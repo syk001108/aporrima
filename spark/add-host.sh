33a4e25d4c059ba425a8ca2494280e5bd2f1410c
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-23 15:13:27 +0000

    REFACTOR: Refactor script for standalone cluster
    Refactor add-host.sh for setting up standalone cluster

--- spark/add-host.sh
+++ spark/add-host.sh
@@ -1,22 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
+MASTER_IP=$(/sbin/ifconfig | grep '\<inet\>' | sed -n '1p' | tr -s ' ' | cut -d ' ' -f3 | cut -d ':' -f2)
+MASTER_HOST=spark-master
 ETC_HOSTS=/etc/hosts
-HOSTNAME=$1
-IP=$2
-HOSTS_LINE="$IP\t$HOSTNAME"
+IP=$1
 
-if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
-then
-    echo "$HOSTNAME already exists : $(grep $HOSTNAME $ETC_HOSTS)"
-    ./aporrima/spark/remove-host.sh $HOSTNAME
-fi
+echo "This script will set up /etc/hosts file"
+read -r -p "Please input the number of walker nodes: " NUM
+NUM=$(($NUM+1))
+echo $NUM
+for ((var=0 ; var < $NUM ; var++));
+do
+    if [ $var -eq 0 ]; then
+        cat <<EOF | sudo tee /etc/hosts
+$MASTER_IP      $MASTER_HOST
+EOF
+    else
+        read -r -p "Please input IP: " IP
+        HOSTNAME="spark-worker$var"
+        HOSTS_LINE="$IP\t$HOSTNAME"
 
-echo "Adding $HOSTNAME to your $ETC_HOSTS";
-sudo -- sh -c -e "echo '$HOSTS_LINE' >> /etc/hosts";
-
-if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
-then
-    echo "$HOSTNAME was added succesfully \n $(grep $HOSTNAME /etc/hosts)";
-else
-    echo "Failed to Add $HOSTNAME, Try again!";
-fi
+        if [ -n "$(grep $HOSTNAME /etc/hosts)" ]; then
+            echo "$HOSTNAME already exists : $(grep $HOSTNAME $ETC_HOSTS)"
+            ./aporrima/spark/remove-host.sh $HOSTNAME
+        else
+            echo "Adding $HOSTNAME to your $ETC_HOSTS"
+            sudo -- sh -c -e "echo '$HOSTS_LINE' >> /etc/hosts"
+        fi
+    # ./aporrima/spark/add-host.sh $HOST $IP
+    fi
+done 
