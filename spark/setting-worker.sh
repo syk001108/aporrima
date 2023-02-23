0b7471abc02b37d283f1c22bd35fd5ed217429d7
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-23 15:22:25 +0000

    REFACTOR: Refactor script for standalone cluster
    Refactor setting-worker.sh for setting up
    standalone cluster

--- spark/setting-worker.sh
+++ spark/setting-worker.sh
@@ -2,51 +2,19 @@
 
 sudo apt-get update
 sudo apt install openssh-server openssh-client -y
-sudo apt install net-tools
+sudo apt install net-tools -y
 
-# sudo sed -i "/PasswordAuthentication/ c\PasswordAuthentication yes" /etc/ssh/sshd_config
-# sudo sed -i "/PermitRootLogin/ c\PermitRootLogin yes" /etc/ssh/sshd_config
-# sudo systemctl restart sshd
+sudo sed -i "/PasswordAuthentication/ c\PasswordAuthentication yes" /etc/ssh/sshd_config
+sudo sed -i "/PermitRootLogin/ c\PermitRootLogin yes" /etc/ssh/sshd_config
+sudo systemctl restart sshd
 
-# cat <<EOF > ~/.bash_profile
-# source ~/.bashrc
-# EOF
-
-echo "DISCLAIMER: This is an automated script for installing Spark but you should feel responsible for what you're doing!"
-echo "This script will install Spark to your home directory, modify your PATH, and add environment variables to your SHELL config file"
-read -r -p "Proceed? [y/N] " response
-if [[ ! $response =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    echo "Aborting..."
-    exit 1
-fi
-
-echo "This script will create a new Spark user"
-read -r -p "Proceed? [y/N] " response
-if [[ ! $response =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    echo "Aborting..."
-    exit 1
-fi
 ./aporrima/spark/add-spark-user.sh
 echo -n "spark" | su - spark -c "git clone https://github.com/boanlab/aporrima.git"
-
 sleep 1
-echo "This script will install a JAVA&PYTHON3 for Spark"
-read -r -p "Proceed? [y/N] " response
-if [[ ! $response =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    echo "Aborting..."
-    exit 1
-fi
 
 echo "Start install JAVA"
 echo -n "spark" | su - spark -c "./aporrima/spark/install-java.sh"
-
 sleep 1
 
 echo "Start install Python3"
 echo -n "spark" | su - spark -c "./aporrima/spark/install-python.sh"
-
-# echo "Set up ubuntu password"
-# echo 'ubuntu:ubuntu' | sudo chpasswd
