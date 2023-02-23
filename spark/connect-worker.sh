3b5e359216fd93d2eeae44aa48df28d7fcba8224
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-23 18:36:27 +0000

    FEAT: Feat feature to change /etc/hosts
    Feat feature to change /etc/hosts

--- spark/connect-worker.sh
+++ spark/connect-worker.sh
@@ -11,8 +11,11 @@ cnt=0
 for i in $(sed -n '/spark/p' /etc/hosts)
 do
 	cnt=$(($cnt+1))
-	if [[ "${i} " == *"$(hostname -I)"* ]] || [ $(($cnt%2)) != 0 ];then
+	if [[ "${i} " == *"$(hostname -I)"* ]] || [ $(($cnt%2)) == 0 ];then
 		continue
 	fi
-	ssh-copy-id -i /home/spark/.ssh/id_rsa.pub $i
+	ssh-copy-id -i /home/spark/.ssh/id_rsa.pub spark@$i
+	cat /etc/hosts >> host.txt
+	scp host.txt spark@$i:/home/spark/
+	ssh spark@$i "sudo cp host.txt /etc/hosts"
 done
