6b956a0fb3b5344ef5f8c28209046c178857688f
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-23 18:37:14 +0000

    FIX: Fix uncorrect file path
    Fix uncorrect file path

--- spark/setting-standalone-cluster.sh
+++ spark/setting-standalone-cluster.sh
@@ -8,7 +8,7 @@ MASTER_HOST=spark-master
 sudo mkdir /home/spark/logs
 
 cp spark/conf/spark-defaults.conf.template spark/conf/spark-defaults.conf
-cat <<EOF | sudo tee ./spark-defaults.conf
+cat <<EOF | sudo tee spark/conf/spark-defaults.conf
 spark.master                     spark://$MASTER_IP:7077
 spark.serializer                 org.apache.spark.serializer.KryoSerializer
 spark.eventLog.enabled           true
@@ -16,16 +16,17 @@ spark.eventLog.dir               /home/spark/logs
 EOF
 
 cp spark/conf/spark-env.sh.template spark/conf/spark-env.sh
-cat <<EOF | sudo tee ./spark-env.sh
+cat <<EOF | sudo tee spark/conf/spark-env.sh
 export SPARK_MASTER_IP='$MASTER_IP'
 export SPARK_MASTER=$MASTER_HOST
 EOF
 
 cp spark/conf/workers.template spark/conf/workers
+sed -i '/localhost/d' spark/conf/workers
 for i in $(sed -n '/spark/p' /etc/hosts)
 do
 	if [[ "${i}" == *"worker"* ]];then
-		echo $i > $SPARK_HOME/conf/workers
+		echo $i >> $SPARK_HOME/conf/workers
 	fi
 done
 
