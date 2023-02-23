572a35296f97db8aca2778872edcf2aa70fbe7ea
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-23 18:38:36 +0000

    FEAT: Feat feature to set env for spark user
    Feat feature to set env for spark user

--- spark/setting-worker.sh
+++ spark/setting-worker.sh
@@ -18,3 +18,15 @@ sleep 1
 
 echo "Start install Python3"
 echo -n "spark" | su - spark -c "./aporrima/spark/install-python.sh"
+
+SPARK_PATH=spark-3.3.2-bin-hadoop3
+echo -n "spark" | su - spark -c 'cat <<EOF | sudo tee -a ~/.bashrc
+export PATH=\$SPARK_HOME/bin:\$PATH
+export PYSPARK=/usr/bin/python3
+export PYSPARK_DRIVER_PYTHON=jupyter
+EOF'
+echo -n "spark" | su - spark -c "cat <<EOF | sudo tee -a ~/.bashrc
+export SPARK_HOME=/home/spark/$SPARK_PATH
+export PYSPARK_DRIVER_PYTHON_OPTS='notebook --ip=0.0.0.0'
+EOF"
+echo -n "spark" | su - spark -c "source ~/.bashrc"
