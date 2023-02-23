758dc3c55faa24792486325a44e333b4f897a780
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-23 15:19:20 +0000

    REFACTOR: Refactor script for standalone cluster
    Refactor connect-worker.sh for setting up standalone cluster

--- spark/connect-worker.sh
+++ spark/connect-worker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd /home/spark
-ssh-keygen -t rsa
+ssh-keygen -t rsa -P “”
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/id_rsa
@@ -12,8 +11,8 @@ cnt=0
 for i in $(sed -n '/spark/p' /etc/hosts)
 do
 	cnt=$(($cnt+1))
-	if [[ "${i} " == *"$(hostname -I)"* ]] || [ $(($cnt%2)) == 0 ];then
+	if [[ "${i} " == *"$(hostname -I)"* ]] || [ $(($cnt%2)) != 0 ];then
 		continue
 	fi
-	ssh-copy-id -i /home/spark/.ssh/id_rsa.pub spark@$i
+	ssh-copy-id -i /home/spark/.ssh/id_rsa.pub $i
 done
