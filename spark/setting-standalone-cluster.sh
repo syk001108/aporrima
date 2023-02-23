5c1de4c34f61bcf11431090443e226be41d7341e
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-23 15:21:53 +0000

    REFACTOR: Refactor script for standalone cluster
    Refactor setting-standalone-cluster.sh for setting up
    standalone cluster

--- spark/setting-standalone-cluster.sh
+++ spark/setting-standalone-cluster.sh
@@ -3,27 +3,11 @@
 MASTER_IP=$(/sbin/ifconfig | grep '\<inet\>' | sed -n '1p' | tr -s ' ' | cut -d ' ' -f3 | cut -d ':' -f2)
 MASTER_HOST=spark-master
 
-cat <<EOF | sudo tee /etc/hosts
-$MASTER_IP      $MASTER_HOST
-EOF
-
-echo "This script will set up /etc/hosts file"
-read -r -p "Please input the number of walker nodes: " NUM
-for ((var=0 ; var < $NUM ; var++));
-do
-    read -r -p "Please input IP: " IP
-	WORKER_NUMBER=$(($var+1))
-    HOST="spark-worker$WORKER_NUMBER"
-    ./aporrima/spark/add-host.sh $HOST $IP
-done 
-
-cp ./aporrima/spark/connect-worker.sh /home/spark/
-su - spark -c "./connect-worker.sh"
+./aporrima/spark/connect-worker.sh
 
 sudo mkdir /home/spark/logs
-cd /$SPARK_HOME/conf
 
-cp spark-defaults.conf.template spark-defaults.conf
+cp spark/conf/spark-defaults.conf.template spark/conf/spark-defaults.conf
 cat <<EOF | sudo tee ./spark-defaults.conf
 spark.master                     spark://$MASTER_IP:7077
 spark.serializer                 org.apache.spark.serializer.KryoSerializer
@@ -31,13 +15,13 @@ spark.eventLog.enabled           true
 spark.eventLog.dir               /home/spark/logs
 EOF
 
-cp spark-env.sh.template spark-env.sh
+cp spark/conf/spark-env.sh.template spark/conf/spark-env.sh
 cat <<EOF | sudo tee ./spark-env.sh
 export SPARK_MASTER_IP='$MASTER_IP'
 export SPARK_MASTER=$MASTER_HOST
 EOF
 
-cp workers.template workers
+cp spark/conf/workers.template spark/conf/workers
 for i in $(sed -n '/spark/p' /etc/hosts)
 do
 	if [[ "${i}" == *"worker"* ]];then
