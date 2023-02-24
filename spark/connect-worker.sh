df279472ebc1834d4a5a64f85f01acc0b815672d
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-24 02:47:08 +0000

    STYLE: Commit for test
    Commit for test

--- spark/connect-worker.sh
+++ spark/connect-worker.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 mkdir /home/spark/.ssh
-ssh-keygen -t rsa
-# cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+echo -ne '\n\n\n' |ssh-keygen -t rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
 
 cnt=0
 for i in $(sed -n '/spark/p' /etc/hosts)
 do
 	cnt=$(($cnt+1))
-	if [[ "${i} " == *"$(hostname -I)"* ]] || [ $(($cnt%2)) != 0 ];then
+	if [[ "${i} " == *"$(hostname -I)"* ]] || [ $(($cnt%2)) == 0 ];then
 		continue
 	fi
 	ssh-copy-id -i /home/spark/.ssh/id_rsa.pub spark@$i
