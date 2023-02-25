a2290885d0a7cb260e715ac86b35528f2876fba3
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-25 15:04:13 +0000

    STYLE: Commit for test
    Commit for test

--- spark/connect-worker.sh
+++ spark/connect-worker.sh
@@ -3,6 +3,7 @@
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
+cat /etc/hosts >> host.txt
 
 cnt=0
 for i in $(sed -n '/spark/p' /etc/hosts)
@@ -12,7 +13,6 @@ do
 		continue
 	fi
 	ssh-copy-id -i /home/spark/.ssh/id_rsa.pub spark@$i
-	cat /etc/hosts >> host.txt
 	scp host.txt spark@$i:/home/spark/
 	ssh spark@$i "sudo cp host.txt /etc/hosts"
 done
