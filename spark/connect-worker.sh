0caf2a0a85102f5927514404a383d6598808de9a
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-26 16:24:02 +0000

    STYLE: Commit for test
    Commit for test

--- spark/connect-worker.sh
+++ spark/connect-worker.sh
@@ -14,7 +14,7 @@ do
 	if [[ "${i} " == *"$(hostname -I)"* ]] || [ $(($cnt%2)) == 0 ];then
 		continue
 	fi
-	cat ~/.ssh/id_rsa.pub | ssh-copy-id -i /home/spark/.ssh/id_rsa.pub spark@$i
+	ssh-copy-id -i /home/spark/.ssh/id_rsa.pub -o StrictHostKeyChecking=no spark@$i
 	scp -o StrictHostKeyChecking=no host.txt spark@$i:/home/spark/
 	ssh -o StrictHostKeyChecking=no spark@$i "sudo cp host.txt /etc/hosts"
 done
