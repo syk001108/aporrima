b41e3042c6862d5ceb653280ae4d67170c504d27
Apdul0329 <shrmsdn2067@gmail.com>
2023-02-26 15:58:02 +0000

    STYLE: Commit for test
    Commit for test

--- spark/connect-worker.sh
+++ spark/connect-worker.sh
@@ -4,7 +4,11 @@ HOSTNAME=$(hostname)
 
 ssh-keygen -t rsa -C "$HOSTNAME" -f ~/.ssh/id_rsa -q -N "" # && cat ~/.ssh/id_rsa.pub
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-chmod 0600 ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub  
+chmod 644 ~/.ssh/authorized_keys
+chmod 644 ~/.ssh/known_hosts
 cat /etc/hosts >> host.txt
 
 cnt=0
@@ -14,7 +18,7 @@ do
 	if [[ "${i} " == *"$(hostname -I)"* ]] || [ $(($cnt%2)) == 0 ];then
 		continue
 	fi
-	ssh-copy-id -o StrictHostKeyChecking=no -v -i /home/spark/.ssh/id_rsa.pub spark@$i
+	ssh-copy-id -o StrictHostKeyChecking=no -i /home/spark/.ssh/id_rsa.pub spark@$i
 	scp -o StrictHostKeyChecking=no host.txt spark@$i:/home/spark/
 	ssh -o StrictHostKeyChecking=no spark@$i "sudo cp host.txt /etc/hosts"
 done
