04cb46ce7dc64127b6c65c3b81e4bc18f732ee31
psy337337 <psy337337@naver.com>
2023-03-01 20:31:03 +0900

    Update install.sh

--- hadoop/install.sh
+++ hadoop/install.sh
@@ -10,7 +10,7 @@ fi
 
 # update repo JAVA, ssh, net-tools, sshpass 
 sudo apt-get update
-sudo apt install openjdk-11-jdk -y
+sudo apt install openjdk-8-jdk -y
 sudo apt install openssh-server openssh-client -y
 sudo apt install net-tools
 sudo apt-get install sshpass
