2bf33652476602b7e7c6c9670a0af1b0c98eeefd
Apdul0329 <shrmsdn2067@gmail.com>
2023-03-05 08:59:17 +0000

    REFACTOR: Refactor script
    Refactor script

--- spark/install.sh
+++ spark/install.sh
@@ -1,48 +1,27 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt install openssh-server openssh-client -y
-sudo apt install net-tools -y
-sudo apt install sshpass -y
-
-sudo sed -i "/PasswordAuthentication/ c\PasswordAuthentication yes" /etc/ssh/sshd_config
-sudo sed -i "/PermitRootLogin/ c\PermitRootLogin yes" /etc/ssh/sshd_config
-sudo systemctl restart sshd
-
-./aporrima/spark/add-spark-user.sh
-# echo -n "spark" | su - spark -c "git clone https://github.com/boanlab/aporrima.git"
-echo -n "spark" | su - spark -c "git clone https://github.com/Apdul0329/aporrima.git"
-sleep 1
-
-echo "Start install JAVA"
-echo -n "spark" | su - spark -c "./aporrima/spark/install-java.sh"
-
-sleep 1
-
-echo "Start install Python3"
-echo -n "spark" | su - spark -c "./aporrima/spark/install-python.sh"
-
-sleep 1
-
-echo "Start install Spark"
-echo -n "spark" | su - spark -c "./aporrima/spark/install-spark.sh"
-
-
-
 echo "Please select a number that corresponds to the desired installation mode"
-read -r -p "(1 : Local Standalone / 2 : Spark on YARN / 3 : Standalone Cluster) : " response
+read -r -p "(1 : Local Standalone / 2 : Standalone Cluster / 3 : Spark on YARN ) : " response
+./aporrima/spark/setting-prev.sh
+
 case $response in
     1)
         echo "Set up local standalone mode"
+        echo "Start install Spark"
+        echo -n "spark" | su - spark -c "./aporrima/spark/install-spark.sh"
         echo -n "spark" | su - spark -c "./aporrima/spark/setting-standalone.sh"
         ;;
     2)
-        echo "Set up Spark on YARN mode"
-        echo -n "spark" | su - spark -c "./aporrima/spark/setting-spark-on-yarn.sh"
-        ;;
-    3)
         echo "Set up standalone cluster mode"
         ./aporrima/spark/add-host.sh
+        echo -n "spark" | su - spark -c "./aporrima/spark/install-spark.sh"
         echo -n "spark" | su - spark -c "./aporrima/spark/setting-standalone-cluster.sh"
         echo -n "spark" | su - spark -c "./spark/sbin/start-all.sh"
+        ;;
+    3)
+        echo "Set up Spark on YARN mode"
+        echo "Start install Spark"
+        echo -n "hadoop" | su - hadoop -c "./aporrima/spark/install-spark.sh"
+        echo -n "hadoop" | su - hadoop -c "./aporrima/spark/setting-spark-on-yarn.sh"
+        ;;
 esac
