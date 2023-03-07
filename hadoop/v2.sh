ea5d5423eb02250c1189baa20146926dd7e9c088
psy337337 <psy337337@naver.com>
2023-03-07 15:52:27 +0900

    Update v2.sh

--- hadoop/v2.sh
+++ hadoop/v2.sh
@@ -31,12 +31,12 @@ do
         continue
     fi
     sshpass -p $passwd ssh ubuntu@$i -o StrictHostKeyChecking=no "sudo apt-get install git"
-    sshpass -p $passwd ssh ubuntu@$i -o StrictHostKeyChecking=no "git clone https://github.com/psy337337/aporrima.git; ./aporrima/hadoop/step1to3.sh $all"
+    sshpass -p $passwd ssh ubuntu@$i -o StrictHostKeyChecking=no "git clone https://github.com/boanlab/aporrima.git; ./aporrima/hadoop/step1to3.sh $all"
 done
 
 
 # connect NameNode
-sshpass -p hadoop ssh hadoop@hdn -o StrictHostKeyChecking=no -t "cd; git clone https://github.com/psy337337/aporrima.git; ./aporrima/hadoop/connect.sh"
+sshpass -p hadoop ssh hadoop@hdn -o StrictHostKeyChecking=no -t "cd; git clone https://github.com/boanlab/aporrima.git; ./aporrima/hadoop/connect.sh"
 
 
 # connect DataNode
@@ -47,7 +47,7 @@ do
         num=$(($num+1))
         continue
     fi
-    sshpass -p hadoop ssh hadoop@$i -o StrictHostKeyChecking=no -t "cd; git clone https://github.com/psy337337/aporrima.git; ./aporrima/hadoop/connect.sh"
+    sshpass -p hadoop ssh hadoop@$i -o StrictHostKeyChecking=no -t "cd; git clone https://github.com/boanlab/aporrima.git; ./aporrima/hadoop/connect.sh"
 done
 
 
